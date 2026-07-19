1b907b79b94a8719257a0b93902299ed139c917b
r7tiiu <thienan190609@gmail.com>
2026-07-19 18:15:13 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 /* Cấu hình chung cho hệ thống StressCheck AI */
 const CONFIG = {
-    VERSION: "3.1",
+    VERSION: "3.2",
+    APP_VERSION: "1.0.1",
     STORAGE_KEY: "stresscheck_results",
     CRITICAL_THRESHOLD: 70,
     MAX_SCORE_PER_QUESTION: 4,
